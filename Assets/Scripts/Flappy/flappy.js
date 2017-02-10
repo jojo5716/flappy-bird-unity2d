3f7875c8d11cd810d76b60737cdd553f9ee9465e
Jonathan Rodriguez <jonathanrodriguez@Jonathans-MacBook-Pro.local>
2017-02-10 22:16:22 +0100

    score points

--- Assets/Scripts/Flappy/flappy.js
+++ Assets/Scripts/Flappy/flappy.js
@@ -3,14 +3,24 @@
 public var gravity : Vector3;
 public var velocityFlying : Vector3;
 public var maxVelocity : float;
+public var tube1 : MoveTube;
+public var tube2 : MoveTube;
 
 private var isFlying : boolean;
 private var velocity : Vector3;
 
+private var anim : Animator;
+private var gameOver : boolean;
+
+function Start() {
+	anim = this.gameObject.GetComponent.<Animator>();
+}
 
 function Update () {
 	if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
-		isFlying = true;
+		if (!gameOver) {
+			isFlying = true;
+		}
 	}
 }
 
@@ -28,7 +38,27 @@ function FixedUpdate() {
 	 if (velocity.y >= 0) {
 	 	angle = Mathf.Lerp(0, 25, velocity.y / maxVelocity);
 	 } else {
-	 	angle = Mathf.Lerp(0, -75, -velocity.y / maxVelocity);
+	 	angle = Mathf.Lerp(0, -25, -velocity.y / maxVelocity);
 	 }
 	 transform.rotation = Quaternion.Euler(0, 0, angle);
+}
+
+function OnCollisionEnter2D(coll : Collision2D) {
+	var collisionName = coll.gameObject.name;
+	var posibleColisions = ['tubeTop', 'tubeBottom', 'floor1'];
+
+	if (collisionName in posibleColisions) {
+		 tube1.velocity = new Vector2(0, 0);
+		 tube2.velocity = new Vector2(0, 0);
+		 anim.SetTrigger('gameOver');
+		 gameOver = true;
+	}
+
+	if (collisionName == 'tubeBottom') {
+    	coll.gameObject.GetComponent.<BoxCollider2D>().enabled = false;
+	}
+
+	if (collisionName == 'floor1') {
+		 gravity = new Vector3(0, 0, 0);
+	}
 }
--- Assets/Scripts/Tubes/MoveTube.js
+++ Assets/Scripts/Tubes/MoveTube.js
@@ -2,6 +2,9 @@
 
 public var velocity : Vector2;
 public var distanceBetweenColumns: Vector2;
+public var score : UI.Text;
+
+private var addScore : boolean = true;
 
 function Update () {
 	moveTube();		
@@ -14,5 +17,12 @@ function moveTube() {
 		var temporalPosition : Vector2 = this.transform.position + distanceBetweenColumns;
 		temporalPosition.y = Random.Range(-3f, 0.6f);
 		this.transform.position = temporalPosition;
+		addScore = true;
+	}
+
+	if (this.transform.position.x <= -12.83f && addScore) {
+		var points = int.Parse(score.text) + 1;
+		score.text = points.ToString();
+		addScore = false;
 	}
 }
